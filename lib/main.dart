bd28700ab6e1b32d3f8dd5432d2714a4926a7ecf
Rishabh-alt-hash <rishabhsingh6953@gmail.com>
2022-10-04 15:38:55 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:flutter_application_1/pages/login_page.dart';
+
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,10 +12,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isMale = true;
+    num temp = 30.5;
+
+    var day = "Monday";
+    const pi = 3.14;
+
     return MaterialApp(
-      home: Material(
-        child: Text("Welcome to 30 days of flutter"),
-      ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
