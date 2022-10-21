4ecc5b6a3aa35a0b26138712df40053f90568c22
Rishabh-alt-hash <rishabhsingh6953@gmail.com>
2022-10-21 15:10:22 +0530

    day-10 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,21 +1,31 @@
+import 'package:form_field_validator/form_field_validator.dart';
+
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "Apple iphone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://cdn.shopify.com/s/files/1/1684/4603/products/iphone-12-pro-max_Graphite.png?v=1650691600")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
-}
-
-final products = [
   Item(
-      id: "Codepur001",
-      name: "iPhone 12 Pro",
-      desc: "Apple iphone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://cdn.shopify.com/s/files/1/1684/4603/products/iphone-12-pro-max_Graphite.png?v=1650691600")
-];
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
