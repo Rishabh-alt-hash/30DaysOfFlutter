84a03035a82c49948e00bf344e3d1f420b52c3a3
Rishabh-alt-hash <rishabhsingh6953@gmail.com>
2022-11-25 17:51:48 +0530

    day-14 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,11 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_application_1/models/catalog.dart';
-import 'package:flutter_application_1/widgets/themes.dart';
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:velocity_x/velocity_x.dart';
+
+import 'package:flutter_application_1/models/catalog.dart';
+import 'package:flutter_application_1/widgets/themes.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
